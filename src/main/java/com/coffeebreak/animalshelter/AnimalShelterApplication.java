25748840ce5870b6c62ad06009ae068361ecb846
porovoz <falkon1@list.ru>
2023-07-19 12:41:26 +0300

    initial commit

---
+++ src/main/java/com/coffeebreak/animalshelter/AnimalShelterApplication.java
@@ -0,0 +1,13 @@
+package com.coffeebreak.animalshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coffeebreak/animalshelter/AnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeebreak.animalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
