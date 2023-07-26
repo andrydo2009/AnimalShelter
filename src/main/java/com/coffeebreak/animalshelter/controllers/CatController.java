9e00601bafaabb4a3c83bc429769f1275c65df97
porovoz <falkon1@list.ru>
2023-07-26 16:28:05 +0300

    Созданиe CRUD-метода для получения списка всех сущностей Cat

--- src/main/java/com/coffeebreak/animalshelter/controllers/CatController.java
+++ src/main/java/com/coffeebreak/animalshelter/controllers/CatController.java
@@ -1,4 +1,64 @@
 package com.coffeebreak.animalshelter.controllers;
 
+import com.coffeebreak.animalshelter.models.Cat;
+import com.coffeebreak.animalshelter.services.CatService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/cat")
+@Tag(name = "Cats", description = "CRUD-operations to work with the cats")
 public class CatController {
+    private final CatService catService;
+    public CatController(CatService catService) {
+        this.catService = catService;
+    }
+
+    @GetMapping
+    @Operation(
+            summary = "Find all cats",
+            description = "Show all cats"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Cats were successfully found",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema =
+                                    @Schema(implementation = Cat.class))
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Cats were not found",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema =
+                                    @Schema(implementation = Cat.class))
+                            )
+                    }
+            )
+    })
+    public ResponseEntity<Collection<Cat>> getAllCats() {
+        Collection<Cat> cats = catService.findAllCats();
+        if (cats == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cats);
+    }
 }
--- src/main/java/com/coffeebreak/animalshelter/services/CatService.java
+++ src/main/java/com/coffeebreak/animalshelter/services/CatService.java
@@ -1,4 +1,20 @@
 package com.coffeebreak.animalshelter.services;
 
+import com.coffeebreak.animalshelter.models.Cat;
+import com.coffeebreak.animalshelter.repositories.CatRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
 public class CatService {
+    private final CatRepository catRepository;
+
+    public CatService(CatRepository catRepository) {
+        this.catRepository = catRepository;
+    }
+
+    public Collection<Cat> findAllCats() {
+        return catRepository.findAll();
+    }
 }
