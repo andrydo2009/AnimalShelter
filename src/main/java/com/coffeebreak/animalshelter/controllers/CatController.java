e123927cbafe6d910d7c4c7732eea9be8c8b43a5
Tsiri <tsyrena24@gmail.ru>
2023-07-26 19:28:48 +0700

    Написание CRUD-метода для создания сущности Cat

--- src/main/java/com/coffeebreak/animalshelter/controllers/CatController.java
+++ src/main/java/com/coffeebreak/animalshelter/controllers/CatController.java
@@ -1,4 +1,36 @@
 package com.coffeebreak.animalshelter.controllers;
 
+import com.coffeebreak.animalshelter.models.Cat;
+import com.coffeebreak.animalshelter.services.CatService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cat")
+@Tag(name = "Cats")
 public class CatController {
+    private final CatService catService;
+    @Autowired //для внедрения зависимости CatService в конструктор класса CatController
+    public CatController(CatService catService) {
+        this.catService = catService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cat> getCatById(@PathVariable Long id) {
+        Cat cat = catService.getCatById(id);
+        if (cat!= null) {
+            return ResponseEntity.ok(cat);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Cat> createCat(@RequestBody Cat cat) {
+        Cat createdCat = catService.createCat(cat);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCat);
+    }
 }
--- src/main/java/com/coffeebreak/animalshelter/services/CatService.java
+++ src/main/java/com/coffeebreak/animalshelter/services/CatService.java
@@ -1,4 +1,23 @@
 package com.coffeebreak.animalshelter.services;
 
+import com.coffeebreak.animalshelter.models.Cat;
+import com.coffeebreak.animalshelter.repositories.CatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CatService {
+    private final CatRepository catRepository;
+
+    @Autowired
+    public CatService(CatRepository catRepository) {
+        this.catRepository = catRepository;
+    }
+    public Cat createCat(Cat cat) {
+        return catRepository.save(cat);
+    }
+
+    public Cat getCatById(Long id) {
+        return catRepository.findById(id).orElse(null);
+    }
 }
