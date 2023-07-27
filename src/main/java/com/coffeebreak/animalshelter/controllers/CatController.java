554f9c0e8bb21790a4a16b4198bd02183b1eb6f0
porovoz <74820096+porovoz@users.noreply.github.com>
2023-07-27 13:00:09 +0300

    Update CatController.java

--- src/main/java/com/coffeebreak/animalshelter/controllers/CatController.java
+++ src/main/java/com/coffeebreak/animalshelter/controllers/CatController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
