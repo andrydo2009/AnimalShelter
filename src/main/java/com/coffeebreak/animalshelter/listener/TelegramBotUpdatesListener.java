ebb54711bc5c67ff839b3723c2403feac0b5d5b9
porovoz <falkon1@list.ru>
2023-08-05 15:56:39 +0300

    Добавлены иморты библиотек

--- src/main/java/com/coffeebreak/animalshelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/coffeebreak/animalshelter/listener/TelegramBotUpdatesListener.java
@@ -5,6 +5,8 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
